2b424e4363910671daa2ed5013a38f2a20fc673d
青木 <247687009@qq.com>
2020-04-06 22:29:50 +0800

    init

--- src/main/java/io/qingmu/operator/operatordemo/HelloController.java
+++ src/main/java/io/qingmu/operator/operatordemo/HelloController.java
@@ -1,13 +1,26 @@
 package io.qingmu.operator.operatordemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.context.ShutdownEndpoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+
 public class HelloController {
+
+    @Autowired
+    private ShutdownEndpoint shutdownEndpoint;
 
     @GetMapping
     public String hello() {
         return "hello !!!";
+    }
+
+    @GetMapping("/spring/shutdown")
+    public Map<String, String> shutdown(){
+        return shutdownEndpoint.shutdown();
     }
 }
