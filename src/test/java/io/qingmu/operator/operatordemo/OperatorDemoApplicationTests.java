8a1ce7ad05d9905c90981faee3defb15ffb3a546
青木 <247687009@qq.com>
2020-04-06 17:24:36 +0800

    init

---
+++ src/test/java/io/qingmu/operator/operatordemo/OperatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.qingmu.operator.operatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperatorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
