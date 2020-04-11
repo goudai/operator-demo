2d3d2259819e53cacc2fd9901a21f16381498caa
青木 <247687009@qq.com>
2020-04-12 00:50:21 +0800

    init

--- src/main/java/io/qingmu/operator/operatordemo/ShutdownController.java
+++ src/main/java/io/qingmu/operator/operatordemo/ShutdownController.java
@@ -5,16 +5,16 @@ import org.springframework.boot.actuate.context.ShutdownEndpoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
 @RestController
-
-public class HelloController {
-
+public class ShutdownController {
     @Autowired
     private ShutdownEndpoint shutdownEndpoint;
 
-    @GetMapping
-    public String hello() {
-        return "hello !!!";
+    @GetMapping("/spring/shutdown")
+    public Map<String, String> shutdown(HttpServletRequest request) {
+        return shutdownEndpoint.shutdown();
     }
-
 }
